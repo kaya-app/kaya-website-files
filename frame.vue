1ed25275165e77611cee993467ad0653cac50450
brianlowzh <brianlow.zh@gmail.com>
2022-06-13 02:11:40 +0800

    Create frame.vue

---
+++ frame.vue
@@ -0,0 +1,272 @@
+<script setup="">
+const props = defineProps({})
+</script>
+
+<template>
+  <div class="frame-wrapper">
+    <div class="frame-sources-container">
+      <div class="frame-sources-list">
+        <div class="frame-sources-item ks-in o1">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517de617bb5036c8a17_adwords.png">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517de617bb5036c8a17_adwords.png">
+        </div>
+        <div class="frame-sources-item ks-in o2">
+          <img class="lg"
+               src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/625095173183e29d52e42833_facebook.png">
+          <img class="lg"
+               src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/625095173183e29d52e42833_facebook.png">
+        </div>
+        <div class="frame-sources-item ks-in o3">
+          <img class="sm"
+               src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517c039477b7ce2ee59_google-analytics.png">
+          <img class="sm"
+               src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517c039477b7ce2ee59_google-analytics.png">
+        </div>
+        <div class="frame-sources-item ks-in o4">
+          <img class="sm"
+               src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517fa994b64694f84f9_hubspot.png">
+          <img class="sm"
+               src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517fa994b64694f84f9_hubspot.png">
+        </div>
+        <div class="frame-sources-item ks-in o5">
+          <img class="sm"
+               src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/6250951765dafa4819aac4fc_linkedin.png">
+          <img class="sm"
+               src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/6250951765dafa4819aac4fc_linkedin.png">
+        </div>
+        <div class="frame-sources-item ks-in o6">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a50c3abd4a9e0d791bbc09_youtube%201.png">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a50c3abd4a9e0d791bbc09_youtube%201.png">
+        </div>
+      </div>
+      <div class="frame-sources-list">
+        <div class="frame-sources-item tktk ks-in2">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/6250951766c242b84ed5b808_tik-tok.png">
+        </div>
+        <div class="frame-sources-item ks-in2">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/625c902b8827f8c9c3cb5b0f_instagram.png">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/625c902b8827f8c9c3cb5b0f_instagram.png">
+        </div>
+        <div class="frame-sources-item ks-in2">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a50c3a5e8f63fe33b39427_google-search-console.svg">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a50c3a5e8f63fe33b39427_google-search-console.svg">
+        </div>
+        <div class="frame-sources-item ks-in2">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/6250951708896807370c554c_airtable-1482122-1254387.png">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/6250951708896807370c554c_airtable-1482122-1254387.png">
+        </div>
+        <div class="frame-sources-item ks-in2">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a50c3be682459a19da90f5_bing%201.png">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a50c3be682459a19da90f5_bing%201.png">
+        </div>
+        <div class="frame-sources-item ks-in2">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/625095171432fa6e6aaf170c_sheets.png">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/625095171432fa6e6aaf170c_sheets.png">
+        </div>
+      </div>
+      <div class="frame-sources-list connection-path">
+        <div class="frame-sources-connection-path-container">
+          <div class="frame-sources-connection-path"></div>
+        </div>
+        <div class="frame-sources-connection-path-container">
+          <div class="frame-sources-connection-path"></div>
+        </div>
+        <div class="frame-sources-connection-path-container">
+          <div class="frame-sources-connection-path"></div>
+        </div>
+        <div class="frame-sources-connection-path-container">
+          <div class="frame-sources-connection-path"></div>
+        </div>
+        <div class="frame-sources-connection-path-container">
+          <div class="frame-sources-connection-path"></div>
+        </div>
+        <div class="frame-sources-connection-path-container">
+          <div class="frame-sources-connection-path"></div>
+        </div>
+      </div>
+      <div class="frame-sources-list connector">
+        <div class="frame-sources-connector-container">
+          <div class="frame-sources-connector"></div>
+        </div>
+        <div class="frame-sources-connector-container">
+          <div class="frame-sources-connector"></div>
+        </div>
+        <div class="frame-sources-connector-container">
+          <div class="frame-sources-connector"></div>
+        </div>
+        <div class="frame-sources-connector-container">
+          <div class="frame-sources-connector"></div>
+        </div>
+        <div class="frame-sources-connector-container">
+          <div class="frame-sources-connector"></div>
+        </div>
+        <div class="frame-sources-connector-container">
+          <div class="frame-sources-connector"></div>
+        </div>
+      </div>
+    </div>
+    <img class="frame-bubbles"
+         src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a4f3654cf117e8bf77105b_bubbles.png">
+    <div class="frame-app-container">
+      <div class="frame-header">
+        <div class="frame-header-logo">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/624203247e3aac20601c4009_kaya-256.png">
+        </div>
+        <div class="frame-header-menu">
+          <div class="frame-header-menu-item home">
+            <p>Home</p>
+          </div>
+          <div class="frame-header-menu-item">
+            <p>Analytics</p>
+          </div>
+          <div class="frame-header-menu-item">
+            <p>Campaign</p>
+          </div>
+          <div class="frame-header-menu-item">
+            <p>Storefront</p>
+          </div>
+          <div class="frame-header-menu-item">
+            <p>Help</p>
+          </div>
+        </div>
+      </div>
+      <div class="frame-section is-top">
+        <div class="frame-section-header-container">
+          <p class="frame-section-header">Top numbers</p>
+          <div class="frame-button">
+            <div class="frame-button-icon">
+              <svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M26 4H22V2H20V4H12V2H10V4H6C4.9 4 4 4.9 4 6V26C4 27.1 4.9 28 6 28H26C27.1 28 28 27.1 28 26V6C28 4.9 27.1 4 26 4ZM26 26H6V12H26V26ZM26 10H6V6H10V8H12V6H20V8H22V6H26V10Z"
+                  fill="black"/>
+              </svg>
+            </div>
+            <p class="frame-button-label">Last 30 days</p>
+          </div>
+        </div>
+        <div class="frame-top-numbers-container">
+          <div class="frame-top-numbers-item">
+            <p class="frame-top-numbers-item-label">Revenue</p>
+            <p class="frame-top-numbers-item-value">218,777.57</p>
+            <div class="frame-top-numbers-item-comparison">
+              <p class="frame-top-numbers-item-comparison-value">Previously $94,746.25</p>
+              <div class="frame-top-numbers-item-comparison-rate">
+                <div class="frame-top-numbers-item-comparison-rate-icon">
+                  <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
+                       xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 6V8H22.59L6 24.59L7.41 26L24 9.41V22H26V6H10Z" fill="black"/>
+                  </svg>
+                </div>
+                <p class="frame-top-numbers-item-comparison-rate-value">+130.91%</p>
+              </div>
+            </div>
+          </div>
+          <div class="frame-top-numbers-item">
+            <p class="frame-top-numbers-item-label">Cost</p>
+            <p class="frame-top-numbers-item-value">$11,060.73</p>
+            <div class="frame-top-numbers-item-comparison">
+              <p class="frame-top-numbers-item-comparison-value">Previously $14,786.26</p>
+              <div class="frame-top-numbers-item-comparison-rate">
+                <div class="frame-top-numbers-item-comparison-rate-icon down">
+                  <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
+                       xmlns="http://www.w3.org/2000/svg">
+                    <path d="M22 26V24H9.41L26 7.41L24.59 6L8 22.59V10H6V26H22Z" fill="black"/>
+                  </svg>
+                </div>
+                <p class="frame-top-numbers-item-comparison-rate-value down">-25.19%</p>
+              </div>
+            </div>
+          </div>
+          <div class="frame-top-numbers-item">
+            <p class="frame-top-numbers-item-label">Return on Ads Spend</p>
+            <p class="frame-top-numbers-item-value">19.78x</p>
+            <div class="frame-top-numbers-item-comparison">
+              <p class="frame-top-numbers-item-comparison-value">Previously 6.41x</p>
+              <div class="frame-top-numbers-item-comparison-rate">
+                <div class="frame-top-numbers-item-comparison-rate-icon">
+                  <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
+                       xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 6V8H22.59L6 24.59L7.41 26L24 9.41V22H26V6H10Z" fill="black"/>
+                  </svg>
+                </div>
+                <p class="frame-top-numbers-item-comparison-rate-value">+208.58%</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="frame-section is-bottom">
+        <p class="frame-section-header is-secondary">Daily Revenue</p>
+        <div class="frame-main-chart-container">
+          <div class="frame-main-chart">
+            <img
+              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a4e90863aafa4e94e3f69d_area-chart.svg">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="frame-slack-container">
+      <div class="frame-slack-header-container">
+        <p class="frame-slack-header">
+          Daily Roundup
+        </p>
+        <div class="frame-slack-header-peoples">
+          <div class="frame-slack-header-peoples-item">
+            <img
+              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a4f5c75e8f63f618b2f788_people-female.png">
+          </div>
+          <div class="frame-slack-header-peoples-item">
+            <img
+              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a4f5c72b9d4205d07aeea0_people-male.png">
+          </div>
+          <div class="frame-slack-header-peoples-item logo-slack">
+            <img
+              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a4f51fcaa938b0ddcd9053_Slack_Mark.svg">
+          </div>
+        </div>
+      </div>
+      <div class="line-charts-container">
+        <img
+          src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a506295049156085fd024a_line-charts.svg">
+      </div>
+      <div class="frame-slack-tooltip-container">
+        <p class="frame-slack-tooltip-period">Yesterday</p>
+        <div class="frame-slack-tooltip-metric-item">
+          <div class="frame-slack-tooltip-metric-icon">
+          </div>
+          <p class="frame-slack-tooltip-metric-value">
+            ROAS: 17.2x
+          </p>
+        </div>
+        <div class="frame-slack-tooltip-metric-item">
+          <div class="frame-slack-tooltip-metric-icon alt">
+          </div>
+          <p class="frame-slack-tooltip-metric-value">
+            Cost: $350.19
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+//@import "assets/styles/main";
+@import "~@/assets/styles/frame";
+</style>
