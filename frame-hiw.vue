ac8ba12458a9f8a8b6d5f66c2dac977453d14950
brianlowzh <brianlow.zh@gmail.com>
2022-06-15 10:05:23 +0800

    Update frame-hiw.vue

--- frame-hiw.vue
+++ frame-hiw.vue
@@ -1,116 +1,110 @@
 <script setup="">
 
-import { ref } from 'vue'
-
 const props = defineProps({})
-const x = ref(false)
 </script>
 
 <template>
-  <div class="frame-hiw-wrapper" :class="x && 'with-kaya'">
-    <div class="frame-hiw-parent-container">
-      <div class="frame-hiw-sources-container">
-        <div class="frame-hiw-sources-list row1">
-          <div class="frame-hiw-sources-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517de617bb5036c8a17_adwords.png">
+    <div class="frame-hiw-wrapper with-kaya">
+      <div class="frame-hiw-parent-container">
+        <div class="frame-hiw-sources-container">
+          <div class="frame-hiw-sources-list row1">
+            <div class="frame-hiw-sources-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517de617bb5036c8a17_adwords.png">
+            </div>
+            <div class="frame-hiw-sources-item">
+              <img class="lg"
+                   src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/625095173183e29d52e42833_facebook.png">
+            </div>
+            <div class="frame-hiw-sources-item">
+              <img class="sm"
+                   src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517c039477b7ce2ee59_google-analytics.png">
+            </div>
+            <div class="frame-hiw-sources-item">
+              <img
+                class="xl"
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8c927211d2610cd10c2f1_add.svg">
+            </div>
           </div>
-          <div class="frame-hiw-sources-item">
-            <img class="lg"
-                 src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/625095173183e29d52e42833_facebook.png">
-          </div>
-          <div class="frame-hiw-sources-item">
-            <img class="sm"
-                 src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517c039477b7ce2ee59_google-analytics.png">
-          </div>
-          <div class="frame-hiw-sources-item">
-            <img
-              class="xl"
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8c927211d2610cd10c2f1_add.svg">
-          </div>
-        </div>
-        <div class="frame-hiw-sources-list row2">
-          <div class="frame-hiw-sources-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/6250951766c242b84ed5b808_tik-tok.png">
-          </div>
-          <div class="frame-hiw-sources-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/625095171432fa6e6aaf170c_sheets.png">
-          </div>
-          <div class="frame-hiw-sources-item">
-            <img class="sm"
-                 src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/6250951765dafa4819aac4fc_linkedin.png">
-          </div>
-          <div class="frame-hiw-sources-item">
-            <img class="sm"
-                 src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517fa994b64694f84f9_hubspot.png">
-          </div>
-        </div>
-      </div>
-      <div class="arrow-container primary">
-        <div class="arrow"></div>
-        <div class="desc"></div>
-      </div>
-      <div class="frame-hiw-excel-container">
-        <div class="frame-hiw-excel-list row1">
-          <div class="frame-hiw-excel-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
-          </div>
-          <div class="frame-hiw-excel-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
-          </div>
-          <div class="frame-hiw-excel-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
-          </div>
-          <div class="frame-hiw-excel-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
-          </div>
-          <div class="frame-hiw-excel-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+          <div class="frame-hiw-sources-list row2">
+            <div class="frame-hiw-sources-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/6250951766c242b84ed5b808_tik-tok.png">
+            </div>
+            <div class="frame-hiw-sources-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/625095171432fa6e6aaf170c_sheets.png">
+            </div>
+            <div class="frame-hiw-sources-item">
+              <img class="sm"
+                   src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/6250951765dafa4819aac4fc_linkedin.png">
+            </div>
+            <div class="frame-hiw-sources-item">
+              <img class="sm"
+                   src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62509517fa994b64694f84f9_hubspot.png">
+            </div>
           </div>
         </div>
-        <div class="frame-hiw-excel-list row2">
-          <div class="frame-hiw-excel-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+        <div class="arrow-container primary">
+          <div class="arrow"></div>
+          <div class="desc"></div>
+        </div>
+        <div class="frame-hiw-excel-container">
+          <div class="frame-hiw-excel-list row1">
+            <div class="frame-hiw-excel-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+            </div>
+            <div class="frame-hiw-excel-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+            </div>
+            <div class="frame-hiw-excel-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+            </div>
+            <div class="frame-hiw-excel-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+            </div>
+            <div class="frame-hiw-excel-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+            </div>
           </div>
-          <div class="frame-hiw-excel-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
-          </div>
-          <div class="frame-hiw-excel-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
-          </div>
-          <div class="frame-hiw-excel-item">
-            <img
-              src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+          <div class="frame-hiw-excel-list row2">
+            <div class="frame-hiw-excel-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+            </div>
+            <div class="frame-hiw-excel-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+            </div>
+            <div class="frame-hiw-excel-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+            </div>
+            <div class="frame-hiw-excel-item">
+              <img
+                src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a8b15cde575a72e5611361_excel-pencil-style.svg">
+            </div>
           </div>
         </div>
-      </div>
-      <div class="frame-hiw-kaya-logo">
-        <img
-          src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/624203247e3aac20601c4009_kaya-256.png">
-      </div>
-      <div class="arrow-container secondary">
-        <div class="arrow"></div>
-        <div class="desc">Transforms to</div>
-      </div>
-      <div class="frame-hiw-channels-table">
-        <img
-          src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a90098dde5e72f93bbbd61_pd-tables-charts.svg"/>
+        <div class="frame-hiw-kaya-logo">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/624203247e3aac20601c4009_kaya-256.png">
+        </div>
+        <div class="arrow-container secondary">
+          <div class="arrow"></div>
+          <div class="desc">Transforms to</div>
+        </div>
+        <div class="frame-hiw-channels-table">
+          <img
+            src="https://uploads-ssl.webflow.com/61dd865bd089adaaa43f7154/62a93dc59a8a981f4c5e9554_pd-tables-chart.png"/>
+        </div>
       </div>
     </div>
-    <teleport to="body">
-      <button @click="x = !x">Click</button>
-    </teleport>
-  </div>
 </template>
 
 <style lang="scss" scoped>
