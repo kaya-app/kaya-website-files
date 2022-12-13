4177fcbf22712e61bb9dc642c9558cd3ab4776dd
brianlowzh <brian@usekaya.com>
2022-12-13 13:54:37 +0800

    Update tf-lead-form.js

--- tf-lead-form.js
+++ tf-lead-form.js
@@ -1,25 +1,34 @@
-// saved in WF form
+// saved in WF form, not used in TF
 const hiddenInput = document.querySelectorAll('.hidden-current-page')
 hiddenInput.forEach(i => {
   i.value = location.href;
 })
 
-const TFLaunchers = document.querySelectorAll('.tf-form')
-TFLaunchers.forEach(i => {
+const historyReplaceTo = '?demo-request-started=true'
+const hrefSource = location.href
+
+// for CTAs within WF forms
+const TFLaunchersWithinForm = document.querySelectorAll('.tf-form')
+TFLaunchersWithinForm.forEach(i => {
   i.addEventListener('submit', function() {
     const _input = i.querySelector('.input-email')
-    // const _input = i.previousElementSibling
-    const _hrefSource = location.href
     const _queryString = window.location.search
     const formId = i.getAttribute('tf-form-id')
-    const historyReplaceTo = i.getAttribute('tf-history-replace')
     window.tf.createPopup(formId, {
       hidden: {
         email: _input.value,
         url_params: _queryString,
-        href_source: _hrefSource
+        href_source: hrefSource
       }
     }).open()
     window.history.replaceState(null, null, historyReplaceTo)
   })
+})
+
+const TFLaunchers = document.querySelectorAll('[data-tf-prop]')
+TFLaunchers.forEach(i => {
+  i.setAttribute('data-tf-hidden', `source=${hrefSource}`)
+  i.addEventListener('click', function () {
+    window.history.replaceState(null, null, historyReplaceTo)
+  }
 })
