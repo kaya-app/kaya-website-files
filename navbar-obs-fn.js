f49b3161938c5e15ca20da1e9d498a2eddb22114
brianlowzh <brian@usekaya.com>
2024-09-30 01:22:08 +0800

    Update navbar-obs-fn.js

--- navbar-obs-fn.js
+++ navbar-obs-fn.js
@@ -1,22 +1,20 @@
 function navbarIntersectionObserver(elId) {
-const targetElement = document.getElementById(elId)
-const navbarElement = document.getElementById("navbar")
+  const targetElement = document.getElementById(elId)
+  const navbarElement = document.getElementById("navbar")
 
-if (!targetElement || !navbarElement) return
+  if (!targetElement || !navbarElement) return
 
-const options = {
-  root: null, // use the viewport as the root
-  rootMargin: "0px",
-  threshold: 0, // trigger when 10% of the target is visible
+  const options = {
+    root: null, // use the viewport as the root
+    rootMargin: "0px",
+    threshold: 0, // trigger when 10% of the target is visible
+  }
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) navbarElement.classList.add("intersect-in-view")
+      else navbarElement.classList.remove("intersect-in-view")
+    })
+  }, options)
+  observer.observe(targetElement)
 }
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) navbarElement.classList.add("intersect-in-view")
-    else navbarElement.classList.remove("intersect-in-view")
-  })
-}, options)
-observer.observe(targetElement)
-}
-
-document.addEventListener("DOMContentLoaded", navbarIntersectionObserver)
