313ea16c5eb3540bfdac6de0bc4c15dcb0faa271
brianlowzh <brian@usekaya.com>
2024-09-30 01:20:33 +0800

    Create navbar-obs-fn.js

---
+++ navbar-obs-fn.js
@@ -0,0 +1,22 @@
+function navbarIntersectionObserver(elId) {
+const targetElement = document.getElementById(elId)
+const navbarElement = document.getElementById("navbar")
+
+if (!targetElement || !navbarElement) return
+
+const options = {
+  root: null, // use the viewport as the root
+  rootMargin: "0px",
+  threshold: 0, // trigger when 10% of the target is visible
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) navbarElement.classList.add("intersect-in-view")
+    else navbarElement.classList.remove("intersect-in-view")
+  })
+}, options)
+observer.observe(targetElement)
+}
+
+document.addEventListener("DOMContentLoaded", navbarIntersectionObserver)
