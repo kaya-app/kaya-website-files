902f6e2679c0b4152b783aff7ed0557ee5e3a328
brianlowzh <brian@usekaya.com>
2024-09-30 02:45:17 +0800

    Update navbar-obs-fn.js

--- navbar-obs-fn.js
+++ navbar-obs-fn.js
@@ -4,17 +4,18 @@ function navbarIntersectionObserver(elId, isLight) {
 
   if (!targetElement || !navbarElement) return
 
+  const _threshold = 0.1
   const options = {
     root: null, // use the viewport as the root
     rootMargin: "0px",
-    threshold: 0.1,
+    threshold: _threshold,
   }
 
   let classesToToggle = ["intersect-in-view"]
   if (isLight) classesToToggle = ["intersect-in-view", "intersect-light"]
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      if (entry.isIntersecting) navbarElement.classList.add(...classesToToggle)
+      if (entry.intersectionRatio >= _threshold) navbarElement.classList.add(...classesToToggle)
       else navbarElement.classList.remove(...classesToToggle)
     })
   }, options)
